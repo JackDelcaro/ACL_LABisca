76e591d6a602b283b3c954023b777ed690ed7edf
Alessandro-Colombo <alessandro50.colombo@mail.polimi.it>
2022-03-09 15:47:52 +0100

    model updated(?)

--- Sandbox/Ale/aaarrf.m
+++ Sandbox/Ale/aaarrf.m
@@ -1,33 +1,40 @@
-clear vars
+clearvars
 
+%% syms for days
 
 syms l1 l2 real;
-syms al th real;
+syms al th th_0 real;
 syms al_dot th_dot real;
 syms al_ddot th_ddot real;
-syms mp mr Lp Lr Jm Jh real;
+syms m2 m1 Lp Lr L1 L2 Jm Jh real;
 syms Cth Cal real;
 syms tau T Attr real;
-syms th_dh th_dh_dot th_dh_ddot c g G C real;
+syms c g G C K real;  
 
-L1 = Lr;
-L2 = Lp;
+%% var definition
+%L1 = Lr;
+%L2 = Lp;
+%l1 = L1/2;   %comment these for generic values
+%l2 = L2/2;   %
 
-
-Q = [th_dh al]';
-Q_dot = [th_dh_dot al_dot]';
-Q_ddot = [th_dh_ddot al_ddot]';
+Q = [th al]';
+Q_dot = [th_dot al_dot]';
+Q_ddot = [th_ddot al_ddot]';
 s1 = simplify(sin(Q(1)));
 c1 = simplify(cos(Q(1)));
 s2 = sin(Q(2));
 c2 = cos(Q(2));
 
+%% axes and points
+
 O = [0 0 0]';
 z0 = [0 0 1]';
-z1 = [-c1 s1 0]';
+z1 = [-s1 c1 0]';
 Pl1 = [-l1*s1 l1*c1 0]';
 P1 = [-L1*s1 L1*c1 0]';
 Pl2 = [-L1*s1-l2*c1*s2 L1*c1-l2*s1*s2 -l2*c2]';
+
+%% Jacobians
 
 J0_l1 = [z0 O];
 J0_l2 = [z0 z1];
@@ -35,15 +42,16 @@ Jp_l1 = [cross(z0, Pl1) O];
 Jp_l2 = [cross(z0, Pl2) cross(z1, Pl2-P1)];
 Jp_l2 = simplify(Jp_l2, 10);
 
-I1 = mr*L1^2/12 + Jm + Jh;
-I2 = mp*L2^2/12;
+I1 = m1*L1^2/12 + Jm + Jh;
+I2 = m2*L2^2/12;
 
-B = simplify(mr*(Jp_l1')*Jp_l1 + I1*(J0_l1')*J0_l1 +mp*(Jp_l2')*Jp_l2 + I2*(J0_l2')*J0_l2, 100);
+%% matrixes
+
+B = simplify(m1*(Jp_l1')*Jp_l1 + I1*(J0_l1')*J0_l1 + m2*(Jp_l2')*Jp_l2 + I2*(J0_l2')*J0_l2, 100);
 
 
-% c(i,j,k) = (diff(B(i,j),Q(k)) + diff(B(i,k),Q(j)) - diff(B(j,k),Q(i)))/2;
-
-%c symbles, 2d matrix:
+%c(i,j,k) = (diff(B(i,j),Q(k)) + diff(B(i,k),Q(j)) - diff(B(j,k),Q(i)))/2;
+%c symbols, 2d matrix:(for some reason it didn't work with 3d matrixes)
 %|1  2|
 %|3  4|
 %
@@ -63,13 +71,17 @@ C = simplify(C, 100);
 
 g0=[0 0 -g]';
 
-G = [0 -mp*(g0'*Jp_l2(:,2))]';
+G = [0 -m2*(g0'*Jp_l2(:,2))]';
+
+%% other forces
 
 Attr = [Cth Cal]'.*Q_dot;
 
-T = simplify(B*Q_ddot + C*Q_dot + G + Attr, 1000);
+molla = [-K*(th-th_0) 0]'; %th_0 rest angle of the spring
 
-%tau=T(1)
+%% ta-da
+
+T = simplify(B*Q_ddot + C*Q_dot + G + Attr + molla, 100);
 
 
 

51b9f98574019e69984cba5ac9f819a5cc7ec42b
Alessandro-Colombo <alessandro50.colombo@mail.polimi.it>
2022-03-10 17:56:55 +0100

    correct model

--- Sandbox/Ale/aaarrf.m
+++ Sandbox/Ale/aaarrf.m
@@ -14,8 +14,8 @@ syms c g G C K real;
 %% var definition
 %L1 = Lr;
 %L2 = Lp;
-%l1 = L1/2;   %comment these for generic values
-%l2 = L2/2;   %
+l1 = L1/2;   %comment these for generic values
+l2 = L2/2;   %
 
 Q = [th al]';
 Q_dot = [th_dot al_dot]';
@@ -34,6 +34,20 @@ Pl1 = [-l1*s1 l1*c1 0]';
 P1 = [-L1*s1 L1*c1 0]';
 Pl2 = [-L1*s1-l2*c1*s2 L1*c1-l2*s1*s2 -l2*c2]';
 
+%% Ai
+
+R01 = [c1 0 -s1;
+       s1 0 c1;
+       0 -1 0];
+R12 = [c2 0 -s2;
+       s2 0 c2;
+       0 -1 0];
+   
+R02 = R01*R12;
+
+
+
+
 %% Jacobians
 
 J0_l1 = [z0 O];
@@ -42,13 +56,16 @@ Jp_l1 = [cross(z0, Pl1) O];
 Jp_l2 = [cross(z0, Pl2) cross(z1, Pl2-P1)];
 Jp_l2 = simplify(Jp_l2, 10);
 
-I1 = m1*L1^2/12 + Jm + Jh;
+I1 = m1*L1^2/12;  %add Jh Jm
 I2 = m2*L2^2/12;
+
+I1_big = diag([I1,I1,0]);
+I2_big = diag([I2,I2,0]);
 
 %% matrixes
 
-B = simplify(m1*(Jp_l1')*Jp_l1 + I1*(J0_l1')*J0_l1 + m2*(Jp_l2')*Jp_l2 + I2*(J0_l2')*J0_l2, 100);
-
+%B = simplify(m1*(Jp_l1')*Jp_l1 + I1*(J0_l1')*J0_l1 + m2*(Jp_l2')*Jp_l2 + I2*(J0_l2')*J0_l2, 100);
+B = simplify(m1*(Jp_l1')*Jp_l1 + (J0_l1')*R01*I1_big*R01'*J0_l1 + m2*(Jp_l2')*Jp_l2 + (J0_l2')*R02*I2_big*R02'*J0_l2, 100);
 
 %c(i,j,k) = (diff(B(i,j),Q(k)) + diff(B(i,k),Q(j)) - diff(B(j,k),Q(i)))/2;
 %c symbols, 2d matrix:(for some reason it didn't work with 3d matrixes)

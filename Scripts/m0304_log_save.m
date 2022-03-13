73f4cd950a2a11109c9c0a91745a930e10feeb38
JackDelcaro <giacomo.delcaro@mail.polimi.it>
2022-03-13 15:09:02 +0100

    [ADD]: added cut option to log save

--- Scripts/m0304_log_save.m
+++ Scripts/m0304_log_save.m
@@ -14,9 +14,14 @@ paths.data_folder           = fullfile(string(paths.mainfolder_path), "Data");
 paths.raw_data_folder       = fullfile(string(paths.data_folder), "Raw_Data");
 paths.parsed_data_folder    = fullfile(string(paths.data_folder), "Parsed_Data");
 paths.scripts_folder        = fullfile(string(paths.mainfolder_path), "Scripts");
-addpath(genpath(paths.file_path     ));
-addpath(genpath(paths.data_folder   ));
-addpath(genpath(paths.scripts_folder));
+paths.sim_folder            = fullfile(string(paths.mainfolder_path), "Simulation");
+paths.sim_utils_folder      = fullfile(string(paths.sim_folder), "Utils");
+addpath(genpath(paths.file_path       ));
+addpath(genpath(paths.data_folder     ));
+addpath(genpath(paths.scripts_folder  ));
+addpath(genpath(paths.sim_utils_folder));
+
+run('graphics_options.m');
 
 %% Variable log
 
@@ -34,15 +39,56 @@ for i = 1:length(filename)
         experiment_label = 'test';
     end
     experiment_label = strrep(experiment_label, ' ', '_');
+    
+    time_selection = input('Would you like to select a time interval [Y/N]: ', 's');
+    if any(time_selection == ["Y", "Yes", "y", "YES", "yes"])
+        enable_time_selection = true;
+    else
+        enable_time_selection = false;
+        inf_time_th = 0;
+        sup_time_th = inf;
+    end
 
     load(fullfile(path, filename(i)));
     
     eval("log_var = " + string(strrep(strrep(filename(i), '.mat', ''), '-', '_')) + ";");
-
+    
+    Log_data.time    = log_var(1, :)';
+    Log_data.theta   = log_var(2, :)' *0.176 /180 * pi;
+    Log_data.alpha   = log_var(3, :)' *0.176 /180 * pi;
     Log_data.voltage = log_var(4, :)';
-    Log_data.theta = log_var(2, :)' *0.176 /180 * pi;
-    Log_data.alpha = log_var(3, :)' *0.176 /180 * pi;
-    Log_data.time = log_var(1, :)';
+    
+    if enable_time_selection
+        figure;
+        sgtitle("Experiment: " + string(strrep(strrep(filename(i), ".mat", ""), "_", "\_")));
+
+        sub(1) = subplot(3,1,1);
+        plot(Log_data.time, Log_data.voltage); hold on; grid on;
+        ylabel('$Voltage\;[V]$');
+
+        sub(2) = subplot(3,1,2);
+        plot(Log_data.time, Log_data.theta); hold on; grid on;
+        ylabel('$\theta\;[rad]$');
+
+        sub(3) = subplot(3,1,3);
+        plot(Log_data.time, Log_data.alpha); hold on; grid on;
+        ylabel('$\alpha\;[rad]$');
+        xlabel('$time\;[s]$');
+
+        linkaxes(sub, 'x');
+        
+        inf_time_th = input('Start time: ');
+        inf_time_th = max([0, inf_time_th]);
+        sup_time_th = input('End time: ');
+        sup_time_th = min([Log_data.time(end), sup_time_th]);
+    end
+    
+    % Time interval selection
+    Log_data.voltage = Log_data.voltage(Log_data.time > inf_time_th & Log_data.time < sup_time_th);
+    Log_data.theta   = Log_data.theta(Log_data.time > inf_time_th & Log_data.time < sup_time_th);
+    Log_data.alpha   = Log_data.alpha(Log_data.time > inf_time_th & Log_data.time < sup_time_th);
+    Log_data.time    = Log_data.time(Log_data.time > inf_time_th & Log_data.time < sup_time_th);
+    Log_data.time    = Log_data.time - Log_data.time(1);
     
     savefile_date = get_savefile_date(filename(i));
     savefile_label = savefile_date + string(experiment_label) + ".mat";

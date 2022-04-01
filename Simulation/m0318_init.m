b6e9e5489f090d1fb94c91bb80eb7706b0b91b7f
JackDelcaro <giacomo.delcaro@mail.polimi.it>
2022-04-01 15:45:26 +0200

    [ADD]: added lab 04/01 data

--- Simulation/m0318_init.m
+++ Simulation/m0318_init.m
@@ -58,21 +58,19 @@ dt_control = 2e-3;
 
 % 
 res_theoretical = sqrt(1.5*PARAMS.g/PARAMS.Lp)/2/pi;
-standby_duration = 3; % [s]
+standby_duration = 5; % [s]
 % Steps Parameters
 steps_amplitude = [0.3 0.5 0.8 1];
-steps_duration = 3; % [s]
+steps_duration = 5; % [s]
 % Ramps Parameters
  ramps_amplitude = [1 1];
- ramps_duration = [8 5]; % [s]
+ ramps_duration = [8 5 3]; % [s]
  ramps_backoff_duration = 1;
 % Sine Sweep Parameters
-sweep_params = [0.5 8 50]; % [ initial_frequency [Hz], final_frequency [Hz], duration [s] ]
+sweep_params = [0.7*res_theoretical 1*res_theoretical 100]; % [ initial_frequency [Hz], final_frequency [Hz], duration [s] ]
                 
 [sim_time_th, experiment_th] = input_generator(dt, standby_duration,...
-                               'steps', steps_amplitude, steps_duration,...
-                               'ramps', ramps_amplitude, ramps_duration, ramps_backoff_duration,...
-                               'sweep', sweep_params, 'linear');
+                                'sweep', sweep_params, 'linear');
 
                 
 % [sim_time, experiment] = input_generator(dt, standby_duration,...
@@ -89,7 +87,7 @@ sweep_params = [0.5 8 50]; % [ initial_frequency [Hz], final_frequency [Hz], dur
 % experiment = 0*1.5*ones(size(sim_time));
 
 simin.voltage = [sim_time_th, 10*experiment_th];
-simin.theta_ref = [sim_time_th, pi/2*experiment_th];
+simin.theta_ref = [sim_time_th, pi/4*experiment_th];
 % figure;
 % plot(sim_time,experiment); grid on;
 T_sim = simin.theta_ref(end, 1);
@@ -111,51 +109,51 @@ filt = 1/(s/(2*pi*freq_filter)+1);
 %% REF FILTER
 
 s = tf('s');
-freq_ref_filter = 1;
+freq_ref_filter = 3;
 ref_filt = 1/(s/(2*pi*freq_ref_filter)+1);
 [num_ref_filter, den_ref_filter] = tfdata(c2d(ref_filt, dt_control), 'v');
 
 %% SIMULATION
 
-simout = sim("s0318_main.slx");
-
-%% RESULTS
-
-figure;
-sgtitle("Simulation Results");
-
-sub(1) = subplot(3,1,1);
-plot(simout.voltage.Time, simout.voltage.Data); hold on; grid on;
-ylabel('$Voltage\;[V]$');
-
-sub(2) = subplot(3,1,2);
-plot(simout.theta.Time, simout.theta.Data*180/pi, 'DisplayName', 'Simulated'); hold on; grid on;
-% plot(simout.theta_ref.Time, simout.theta_ref.Data*180/pi, 'DisplayName', 'Reference');
-% plot(log.time, log.theta*180/pi, 'DisplayName', 'Real Data');
-legend;
-ylabel('$\theta\;[deg]$');
-
-sub(3) = subplot(3,1,3);
-plot(simout.alpha.Time, simout.alpha.Data*180/pi, 'DisplayName', 'Simulated'); hold on; grid on;
-% plot(simout.alpha_ref.Time, simout.alpha_ref.Data*180/pi, 'DisplayName', 'Reference');
-% plot(log.time, log.alpha*180/pi, 'DisplayName', 'Real Data');
-legend;
-ylabel('$\alpha\;[deg]$');
-xlabel('$time\;[s]$');
-linkaxes(sub, 'x');
-clearvars sub;
-
-figure
-sub(1) = subplot(3,1,1);
-plot(simout.tau.Time, simout.tau.Data); hold on; grid on;
-ylabel('$\tau\;[Nm]$');
-
-sub(2) = subplot(3,1,2);
-plot(simout.theta_dot.Time, simout.theta_dot.Data*180/pi); hold on; grid on;
-ylabel('$\dot{\theta}\;[deg/s]$');
-
-sub(3) = subplot(3,1,3);
-plot(simout.alpha_dot.Time, simout.alpha_dot.Data*180/pi); hold on; grid on;
-ylabel('$\dot{\alpha}\;[deg/s]$');
-xlabel('$time\;[s]$');
-linkaxes(sub, 'x');
+% simout = sim("s0318_main.slx");
+% 
+% %% RESULTS
+% 
+% figure;
+% sgtitle("Simulation Results");
+% 
+% sub(1) = subplot(3,1,1);
+% plot(simout.voltage.Time, simout.voltage.Data); hold on; grid on;
+% ylabel('$Voltage\;[V]$');
+% 
+% sub(2) = subplot(3,1,2);
+% plot(simout.theta.Time, simout.theta.Data*180/pi, 'DisplayName', 'Simulated'); hold on; grid on;
+% % plot(simout.theta_ref.Time, simout.theta_ref.Data*180/pi, 'DisplayName', 'Reference');
+% % plot(log.time, log.theta*180/pi, 'DisplayName', 'Real Data');
+% legend;
+% ylabel('$\theta\;[deg]$');
+% 
+% sub(3) = subplot(3,1,3);
+% plot(simout.alpha.Time, simout.alpha.Data*180/pi, 'DisplayName', 'Simulated'); hold on; grid on;
+% % plot(simout.alpha_ref.Time, simout.alpha_ref.Data*180/pi, 'DisplayName', 'Reference');
+% % plot(log.time, log.alpha*180/pi, 'DisplayName', 'Real Data');
+% legend;
+% ylabel('$\alpha\;[deg]$');
+% xlabel('$time\;[s]$');
+% linkaxes(sub, 'x');
+% clearvars sub;
+% 
+% figure
+% sub(1) = subplot(3,1,1);
+% plot(simout.tau.Time, simout.tau.Data); hold on; grid on;
+% ylabel('$\tau\;[Nm]$');
+% 
+% sub(2) = subplot(3,1,2);
+% plot(simout.theta_dot.Time, simout.theta_dot.Data*180/pi); hold on; grid on;
+% ylabel('$\dot{\theta}\;[deg/s]$');
+% 
+% sub(3) = subplot(3,1,3);
+% plot(simout.alpha_dot.Time, simout.alpha_dot.Data*180/pi); hold on; grid on;
+% ylabel('$\dot{\alpha}\;[deg/s]$');
+% xlabel('$time\;[s]$');
+% linkaxes(sub, 'x');
